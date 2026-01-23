2c3988fec28629bee280b9c3ef1957de0bdb3303
Matteo Kmeliuskas <matteokmeliuskas@gmail.com>
2026-01-23 16:18:59 -0300

    Ajuste

--- 01_trocador_de_cores/script.js
+++
@@ -1,6 +0,0 @@
-function cliquei() {
-    
-    //Pegar o HTML da Página
-    let corpo = document.getElementById('corpo')
-    corpo.style.background = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-}
